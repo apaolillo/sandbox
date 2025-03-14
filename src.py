0682199f96b00cf748081fb2e07858ea2ac3f747
Antonio Paolillo <apaolill@gmail.com>
2025-03-14 16:59:18 +0100

    Fix the test
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- src.py
+++ src.py
@@ -2,7 +2,7 @@ def add(x, y):
     return x+y
 def test_add():
     assert 5 == add(3, 2)
-    assert 4 == add(2, 1)
+    assert 4 == add(3, 1)
 def main():
     print("hello from main")
 if __name__ == "__main__":
